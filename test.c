beac3ed7562de55c2af8519061a7687c97006a86
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2025-05-18 21:04:06 +0900

    Update test.c

--- test.c
+++ test.c
@@ -2,6 +2,6 @@
 
 main()
 {
-	printf("안녕하세요?\nHave a nice day..\n\n\n");
+	printf("안녕하세요?\nHave a nice day..\n\n\n");  // modified in git : 20250518
 	printf("안녕하세요???????\nHave a nice day..\n\n\n");
 }
