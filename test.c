5a3300bed163d5ba2915ef9868f9ae0955a15c9b
hulklee1 <hulklee@gmail.com>
2025-05-19 10:57:53 +0900

    a

--- test.c
+++ test.c
@@ -2,10 +2,10 @@
 
 main()
 {
-//<<<<<<< HEAD
+
 	printf("안녕하세요?\nHave a nice day..\n\n\n"); // modifird in Raspi
 //=======
 	printf("안녕하세요?\nHave a nice day..\n\n\n");  // modified... in github : 20250519
-//>>>>>>> 6cfe9572edbeba822c2c2b06f96cb2e7fbba1a64
+
 	printf("안녕하세요???????\nHave a nice day..\n\n\n");
 }
