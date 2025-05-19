9dc8149706ac737359b0735708fd88c63961fd24
hulklee1 <hulklee@gmail.com>
2025-05-19 10:44:34 +0900

    2

--- test.c
+++ test.c
@@ -2,10 +2,6 @@
 
 main()
 {
-//<<<<<<< HEAD
-//	printf("안녕하세요?\nHave a nice day..\n\n\n");  // test in ubuntu..............
-//=======
-	printf("안녕하세요?\nHave a nice day..\n\n\n");  // merged... in ubuntu : 20250518
-//>>>>>>> beac3ed7562de55c2af8519061a7687c97006a86
+	printf("안녕하세요?\nHave a nice day..\n\n\n"); // modifird in Raspi
 	printf("안녕하세요???????\nHave a nice day..\n\n\n");
 }
